a83bf5f8d4006064087293e0f12a2e256dfdb3bd
fengwk <759543714@qq.com>
2024-10-09 01:13:03 +0800

    support gruvbox theme

--- config.def.h
+++ config.def.h
@@ -130,6 +130,39 @@ float alpha = 0.85;
 // 	"#1b1b1b", // 默认的背景色
 // };
 
+// gruvbox-flat
+// static const char *colorname[] = {
+//
+// 	/* 8 normal colors */
+// 	"#1D2021", /* black */
+// 	"#ea6962", /* red     */
+// 	"#a9b665", /* green   */
+// 	"#d8a657", /* yellow  */
+// 	"#7daea3", /* blue    */
+// 	"#d3869b", /* magenta */
+// 	"#89b482", /* cyan    */
+// 	"#d4be98", /* white   */
+//  
+//
+// 	/* 8 bright colors */
+//   "#5a524c", /* black   */
+//   "#ea6962", /* red     */
+//   "#a9b665", /* green   */
+//   "#d8a657", /* yellow  */
+//   "#7daea3", /* blue    */
+//   "#d3869b", /* magenta */
+//   "#89b482", /* cyan    */
+//   "#d4be98", /* white   */
+//
+// 	[255] = 0,
+//
+//   /* special colors */
+//   "#ddc7a1", // 光标前景色
+//   "#3c3836", // 光标背景色
+// 	"#d4be98", // 默认的前景色
+// 	"#1d2021", // 默认的背景色
+// };
+
 // catppuccin macchiato
 // https://github.com/catppuccin/st/blob/main/themes/macchiato.h
 static const char *colorname[] = {
